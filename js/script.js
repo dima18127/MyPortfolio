1548cb173dcbc9b6ee0da42a45d2a1eba00b6a40
dima18127 <dima18127@yandex.ru>
2022-12-22 05:48:30 +0400

    Contact_ended

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ scoreSkill.forEach((item,i)=>{
     lines[i].style.width = item.innerHTML
 })
 
+
+// ________________________ scroll intersection api
+// function onEntry(entry) {
+//     entry.forEach(change => {
+//       if (change.isIntersecting) {
+//        change.target.classList.add('element-change');
+//       }
+//     });
+//   }
+  
+//   let options = {
+//     threshold: [1] };
+//   let observer = new IntersectionObserver(onEntry, options);
+//   let elements = document.querySelectorAll('.element-animation');
+  
+//   for (let elm of elements) {
+//     observer.observe(elm);
+//   }
+
